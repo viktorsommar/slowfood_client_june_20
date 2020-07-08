268f29e3b542b9894191233c09ed3f2404b9dcd2
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-08 20:09:16 +0200

    amending menu.jsx sorting products into categories, all tests passing

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
 import axios from "axios";
-
 class Menu extends Component {
   state = {
     menu: [],
   };
-
   componentDidMount = async () => {
     let menuData = await axios.get("/products");
     this.setState({ menu: menuData.data.products });
   };
-
   render() {
-    let menuList;
+    let starters = []
+    let mainCourses = []
+    let deserts = []
     this.state.menu &&
-      (menuList = this.state.menu.map((product) => {
+      (this.state.menu.forEach((product) => {
         if (product.category === "Starters")
-        return (
-          <div id={`product-${product.id}`}>
-            <h3>Starters</h3>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-          </div>
-          );
-          if (product.category === "Main Courses")
-          return(
+          starters.push(
             <div id={`product-${product.id}`}>
-              <h3>Main Courses</h3>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-          </div>
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+            </div>
           );
-          if (product.category === "Deserts")
-          return(
+        if (product.category === "Main Courses")
+          mainCourses.push(
             <div id={`product-${product.id}`}>
-              <h3>Deserts</h3>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-          </div>
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+            </div>
+          );
+        if (product.category === "Deserts")
+          deserts.push(
+            <div id={`product-${product.id}`}>
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+            </div>
           );
       }));
-    return <div>{menuList}</div>;
+    return (
+      <div>
+        <div id="starters">
+          <h3>Starters</h3>
+          {starters}
+        </div>
+        <div id="main-courses">
+          <h3>Main Courses</h3>
+          {mainCourses}
+        </div>
+        <div id="deserts">
+          <h3>Deserts</h3>
+          {deserts}
+        </div>
+      </div>
+    )
   }
 }
-
 export default Menu;
