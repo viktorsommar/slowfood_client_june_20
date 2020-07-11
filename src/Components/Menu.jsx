468561525b563353c1084f0c3cdb1d4aba3d6c98
felixoppongwillborg <fwlixw@gmail.com>
2020-07-11 18:45:22 +0200

    wrote implementationcode for view order

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,52 +1,59 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-
 class Menu extends Component {
   state = {
     menu: [],
     orderMessage: {},
     orderDetails: {},
-    orderId: null
+    orderId: null,
+    showOrder: false,
   };
   componentDidMount = async () => {
-    
     let menuData = await axios.get("/products");
     this.setState({ menu: menuData.data.products });
   };
 
   addToOrder = async (event) => {
-    let productId = event.target.parentElement.dataset.id
-    let credentials = await JSON.parse(sessionStorage.getItem("credentials"))
+    let productId = event.target.parentElement.dataset.id;
+    let credentials = await JSON.parse(sessionStorage.getItem("credentials"));
     let headers = {
       ...credentials,
       "Content-type": "application/json",
-      Accept: "application/json"
-    }
+      Accept: "application/json",
+    };
 
-    let response
-    if (this.state.orderDetails.hasOwnProperty('id')) {
-      response = await axios.put(`/orders/${this.state.orderDetails.id}`, {
-        product_id: productId
-      }, {
-        headers: headers
-      })
+    let response;
+    if (this.state.orderDetails.hasOwnProperty("id")) {
+      response = await axios.put(
+        `/orders/${this.state.orderDetails.id}`,
+        {
+          product_id: productId,
+        },
+        {
+          headers: headers,
+        }
+      );
     } else {
-      response = await axios.post('/orders', {
-        product_id: productId
-      }, {
-        headers: headers
-      })
+      response = await axios.post(
+        "/orders",
+        {
+          product_id: productId,
+        },
+        {
+          headers: headers,
+        }
+      );
     }
 
     this.setState({
       orderMessage: {
         message: response.data.message,
-        id: productId
+        id: productId,
       },
-      orderDetails: response.data.order
-    })
-  }
+      orderDetails: response.data.order,
+    });
+  };
 
   render() {
     let starters = [];
@@ -57,98 +64,120 @@ class Menu extends Component {
       this.state.menu.forEach((product) => {
         if (product.category === "starters")
           starters.push(
-            <div 
-            key={product.id} 
-            id={`product-${product.id}`}
-            data-id={product.id}
-            data-price={product.price}
+            <div
+              key={product.id}
+              id={`product-${product.id}`}
+              data-id={product.id}
+              data-price={product.price}
             >
               {`${product.name} ${product.description} ${product.price}`}
-              {this.props.authenticated && <button id="button" onClick={this.addToOrder}>
-                Add to order
-              </button>}
+              {this.props.authenticated && (
+                <button id="button" onClick={this.addToOrder}>
+                  Add to order
+                </button>
+              )}
 
-            {parseInt(this.state.orderMessage.id) === product.id && 
-              <p id="order-message">{this.state.orderMessage.message}</p>
-            }
+              {parseInt(this.state.orderMessage.id) === product.id && (
+                <p id="order-message">{this.state.orderMessage.message}</p>
+              )}
             </div>
           );
         if (product.category === "main_courses")
           maincourses.push(
-            <div 
-            key={product.id} 
-            id={`product-${product.id}`}
-            data-id={product.id}
-            data-price={product.price}
+            <div
+              key={product.id}
+              id={`product-${product.id}`}
+              data-id={product.id}
+              data-price={product.price}
             >
               {`${product.name} ${product.description} ${product.price}`}
-              {this.props.authenticated && <button id="button" onClick={this.addToOrder}>
-                Add to order
-                </button>}
-                {parseInt(this.state.orderMessage.id) === product.id && 
-              <p id="order-message">{this.state.orderMessage.message}</p>
-            }
+              {this.props.authenticated && (
+                <button id="button" onClick={this.addToOrder}>
+                  Add to order
+                </button>
+              )}
+              {parseInt(this.state.orderMessage.id) === product.id && (
+                <p id="order-message">{this.state.orderMessage.message}</p>
+              )}
             </div>
           );
         if (product.category === "desserts")
           desserts.push(
-            <div 
-            key={product.id} 
-            id={`product-${product.id}`}
-            data-id={product.id}
-            data-price={product.price}
+            <div
+              key={product.id}
+              id={`product-${product.id}`}
+              data-id={product.id}
+              data-price={product.price}
             >
               {`${product.name} ${product.description} ${product.price}`}
-              {this.props.authenticated && <button id="button" onClick={this.addToOrder}>
-                Add to order
-              </button>}
-              {parseInt(this.state.orderMessage.id) === product.id && 
-              <p id="order-message">{this.state.orderMessage.message}</p>
-            }
+              {this.props.authenticated && (
+                <button id="button" onClick={this.addToOrder}>
+                  Add to order
+                </button>
+              )}
+              {parseInt(this.state.orderMessage.id) === product.id && (
+                <p id="order-message">{this.state.orderMessage.message}</p>
+              )}
             </div>
           );
 
         if (product.category === "drinks")
           drinks.push(
-            <div 
-            key={product.id} 
-            id={`product-${product.id}`}
-            data-id={product.id}
-            data-price={product.price}
+            <div
+              key={product.id}
+              id={`product-${product.id}`}
+              data-id={product.id}
+              data-price={product.price}
             >
               {`${product.name} ${product.description} ${product.price}`}
-              {this.props.authenticated && <button id="button" onClick={this.addToOrder}>
-                Add to order
-              </button>}
-              {parseInt(this.state.orderMessage.id) === product.id && 
-              <p id="order-message">{this.state.orderMessage.message}</p>
-            }
+              {this.props.authenticated && (
+                <button id="button" onClick={this.addToOrder}>
+                  Add to order
+                </button>
+              )}
+              {parseInt(this.state.orderMessage.id) === product.id && (
+                <p id="order-message">{this.state.orderMessage.message}</p>
+              )}
             </div>
           );
       });
-    
-    
-   
-    
+
     return (
-      <div>
-        <div id="starters">
-          <h3>Starters</h3>
-          {starters}
+      <>
+        {this.state.orderId !== "" && (
+          <button
+            onClick={() => {
+              this.setState({ showOrder: true });
+            }}
+          >
+            View order
+          </button>
+        )}
+        {this.state.showOrder && (
+          <ul id="order-details">
+            <li>item 1</li>
+            <li>Item 2</li>
+          </ul>
+        )}
+        <div>
+          <div id="starters">
+            <h3>Starters</h3>
+            {starters}
+          </div>
+          <div id="main_courses">
+            <h3>Maincourses</h3>
+            {maincourses}
+          </div>
+          <div id="desserts">
+            <h3>Desserts</h3>
+            {desserts}
+          </div>
+          <div id="drinks">
+            <h3>Drinks</h3>
+            {drinks}
+          </div>
         </div>
-        <div id="main_courses">
-          <h3>Maincourses</h3>
-          {maincourses}
-        </div>
-        <div id="desserts">
-          <h3>Desserts</h3>
-          {desserts}
-        </div>
-        <div id="drinks">
-          <h3>Drinks</h3>
-          {drinks}
-        </div>
-      </div>
+      </>
     );
   }
 }
